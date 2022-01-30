0b2ce4ac9ff5dcf3af74719c00fea71fddc3addb
Smiley <jcsmileyjr@gmail.com>
2022-01-29 21:36:07 -0600

    Develop control section, mobile view

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Refresh from "./image/refresh.png";
+import Play from "./image/play.png";
+import Pause from "./image/pause.png";
+import Mouse from "./image/mouse-animal.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <div className='control'>
+          <button type="button" className='control__button'>
+            <img src={Refresh} alt="" className='control__button--style' />
+            <label>Restart</label>
+          </button>
+          <button type="button" className='control__button'>
+            <img src={Play} alt="" className='control__button--style' />
+            <label>Restart</label>
+          </button>
+          <button type="button" className='control__button'>
+            <img src={Pause} alt="" className='control__button--style' />
+            <label>Restart</label>
+          </button>
+        </div>
+        <div className='content'>
+          <p className='content__timer'>25:00</p>
+          <p className='content__message'>"You may delay, but time will not"</p>
+          <div className='content__animation--container'>
+            <img src={Mouse} alt="" className='content__animal--style' />
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
