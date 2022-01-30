b46060d88910c5e2478a2ab11aa8411f12cff1ff
Smiley <jcsmileyjr@gmail.com>
2022-01-30 09:39:54 -0600

    Refactor displaying of seconds to be more digital looking

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ function App() {
           </button>
         </section>
         <section className='content'>
-          <p className='content__timer'>{minutes}:{seconds === 0?`00`:seconds}</p>
+          <p className='content__timer'>{minutes}:{seconds < 10?`0${seconds}`:seconds}</p>
           <p className='content__message'>You may delay, but time will not</p>
         </section>
         <section className='animation'>
